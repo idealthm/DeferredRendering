1f2e2ae8b02b272a54723f3476f5c789eb3197f1
idealthm <1240568620@qq.com>
2024-02-29 00:42:31 +0800

    #0228 VertexArray | VertexBuffer | IndexBuffer Abstract to Class and uniform data.

---
+++ Source/VertexBuffer/VertexBuffer.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include <vec3.hpp>
+#include <vector>
+#include <glad/glad.h>
+
+class VertexBuffer
+{
+public:
+    VertexBuffer(const void* data, unsigned int size);
+    ~VertexBuffer();
+
+    void Bind() const;
+    void Unbind() const;
+
+private:
+    unsigned int m_RendererID;
+};
